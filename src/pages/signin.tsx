891a911247df18433c578f271ea9e904cc68e087
Matteo <m2lovric@gmail.com>
2020-12-16 12:30:31 +0100

    Add type to signInForm

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {FormEvent, useState} from 'react'
 import Layout from '../components/Layout'
 import { signIn } from '../firebase/auth'
 import { navigate } from 'gatsby'
@@ -7,7 +7,7 @@ const SignIn = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const signInForm = (e: any) => {
+  const signInForm = (e: FormEvent) => {
     e.preventDefault();
     signIn(email, password);
     navigate('/');
@@ -21,9 +21,9 @@ const SignIn = () => {
         </article>
         <form className="sign__form" onSubmit={signInForm} >
           <label htmlFor="email">Email</label>
-          <input type="text" name="email" onChange={(e) => setEmail(e.target.value)}/>
+          <input type="text" name="email" onChange={(e) => setEmail(e.currentTarget.value)}/>
           <label htmlFor="password">Password</label>
-          <input type="password" name="password" onChange={(e) => setPassword(e.target.value)} />
+          <input type="password" name="password" onChange={(e) => setPassword(e.currentTarget.value)} />
           <button type="submit" className="sign__btn red">Sign In</button>
         </form>
       </section>
