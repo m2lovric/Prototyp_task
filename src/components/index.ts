462b2d2fee411b0a3ef29c319e093ea289927cff
Matteo <m2lovric@gmail.com>
2020-12-16 23:17:02 +0100

    Change path

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,5 @@ export {default as Form} from './Form'
 export {default as Layout} from './Layout';
 export {default as List} from './List';
 export {default as Task} from './Task';
-export {default as Header} from './Header/Header';
+export {default as Header} from './Header';
 export {default as Footer} from './Footer';
