340cc89ae365902751ee3030a559981653e24c33
Matteo <m2lovric@gmail.com>
2020-12-16 23:19:01 +0100

    Add done status handler

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,13 +1,28 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { removeTaskAction } from '../redux/actions'
+import { removeTaskAction, handleDoneStatus } from '../redux/actions';
 import { IRootState, ObjectTask } from '../redux/store';
 
 const Task = () => {
-  
-
   const tasks = useSelector<IRootState, ObjectTask[]>((state) => state.task );
   const dispatch = useDispatch();
+
+  const changeDoneStatus = (e: React.SyntheticEvent<HTMLButtonElement>) => {
+    const id = e.currentTarget.dataset.id;
+    console.log(id)
+    const modState = tasks.map((task) => {
+      console.log(task.id)
+      if(id === task.id){
+        return{
+          ...task,
+          done : !task.done
+        }
+      }else{
+        return {...task}
+      }
+    });
+    dispatch(handleDoneStatus(modState));
+  }
 
   const removeTask = (e: React.SyntheticEvent<HTMLButtonElement>) => {
     const id = e.currentTarget.dataset.id;
@@ -25,7 +40,7 @@ const Task = () => {
             <article className="list__task" key={el.id} >
               <p className="list__task__task">{el.task}</p>
               <p className="list__task__dueDate">{el.dueDate}</p>
-              <button className={el.done ? 'green list__task__done' : 'red list__task__done'}>{el.done}</button>
+              <button className={el.done ? 'green list__task__done' : 'red list__task__done'} data-id={el.id} onClick={changeDoneStatus}>{el.done}</button>
               <button className="list__task__remove" data-id={el.id} onClick={removeTask}>Remove</button>
             </article>
           )
