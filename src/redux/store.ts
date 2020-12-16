9c4291c6206daa4b2bba1ee6f7a21bfb0066e2af
Matteo <m2lovric@gmail.com>
2020-12-16 12:31:04 +0100

    Add interfaces to store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,20 @@
 import { createStore as reduxCreateStore } from "redux"
 
-const createStore = (reducer: any, state: []) => reduxCreateStore(reducer, state)
+export interface TaskState {
+  id: string
+  task: string,
+  dueDate: Date,
+  done: boolean
+}
+export interface UserState {
+  uid ?: string,
+  email ?: string
+}
+export interface IRootState {
+  task: TaskState[],
+  user: UserState
+}
+
+const createStore = (reducer: any, state: IRootState) => reduxCreateStore(reducer, state)
 
 export default createStore
