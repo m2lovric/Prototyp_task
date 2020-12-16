13b9cdfef3a914bb8a7062025bbc267908c9f829
Matteo <m2lovric@gmail.com>
2020-12-16 23:19:18 +0100

    Add new cases to reducer

--- src/redux/taskReducer.ts
+++ src/redux/taskReducer.ts
@@ -1,4 +1,4 @@
-import { loadState } from '../localStorage/localStorage.js'
+import { loadState } from '../localStorage/localStorage'
 
 const persistedData = loadState();
 
@@ -8,6 +8,10 @@ export const taskReducer = (state = persistedData || [], action: any) => {
       return [...state, action.data]
     case 'REMOVE':
       return state = action.data
+    case 'DONE':
+      return state = action.data
+    case 'TASK_RESET':
+      return state = []
     default:
       return state
   }
